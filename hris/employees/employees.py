8feec2bde38bd254204646b38092dc58840d7736
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2023-02-10 10:21:50 +0800

    Fixed Routing

--- hris/employees/employees.py
+++ hris/employees/employees.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import current_user
 from hris.models import *
+from .forms import *
 
 employees_bp = Blueprint('employees_bp', __name__,  template_folder='templates',
     static_folder='static', static_url_path='static')
@@ -8,6 +9,7 @@ employees_bp = Blueprint('employees_bp', __name__,  template_folder='templates',
 
 @employees_bp.route('/employees', methods=['GET'])
 def employees():
+   delete_modal = DeleteModal()
    employees = db.session.query(EmployeeInfo.id, EmployeeInfo.last_name, EmployeeInfo.first_name, 
       EmployeeInfo.middle_name, Positions.position_name, Departments.department_name, 
       EmploymentInfo.start_date, EmploymentInfo.salary_package)\
@@ -15,7 +17,7 @@ def employees():
       .join(Positions, Positions.id == EmployeeInfo.position_id)\
       .join(Departments, Departments.id == Positions.department_id).all()
 
-   return render_template('employees.html', employees=employees)
+   return render_template('employees.html', employees=employees, delete_modal = delete_modal)
 
 @employees_bp.route('/employees/add_employee', methods=['GET', 'POST'])
 def add_employee():
@@ -23,12 +25,20 @@ def add_employee():
    return render_template('add_employee.html')
 
 
-@employees_bp.route('/employees/<string:employee_name>-<int:employee_id>', methods=['GET', 'POST'])
+@employees_bp.route('/employees/<string:employee_name>_<int:employee_id>', methods=['GET', 'POST'])
 def manage_employee(employee_name, employee_id):
 
    return render_template('manage_employee.html', employee_name=employee_name, employee_id=employee_id)
 
-@employees_bp.route('/employees/<string:employee_name>-<int:employee_id>', methods=['GET', 'POST'])
-def delete_employee(employee_name, employee_id):
+@employees_bp.route('/employees/delete/<int:employee_id>', methods=['GET', 'POST'])
+def delete_employee(employee_id):
 
-   return render_template('employee.html')
+   employees = db.session.query()\
+      .join(EmployeeInfo, EmployeeInfo.id == EmploymentInfo.employee_id)\
+      .join(Positions, Positions.id == EmployeeInfo.position_id)\
+      .join(Departments, Departments.id == Positions.department_id)\
+      .filter(EmployeeInfo.id == employee_id).delete()
+   db.session.commit()
+
+   return redirect(url_for('employees_bp.employees'))
+   #return render_template('employees.html')
--- hris/employees/forms.py
+++ hris/employees/forms.py
@@ -1,4 +1,6 @@
-# from flask_wtf import FlaskForm
-# from wtforms import PasswordField, StringField, SubmitField, SelectField, EmailField
-# from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError, InputRequired
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField, SelectField, EmailField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError, InputRequired
 
+class DeleteModal(FlaskForm):
+   delete = SubmitField(label='Delete')
