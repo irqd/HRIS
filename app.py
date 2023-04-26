3941b0add0e0a4bb00b05ee1b0c7497710c2e06f
Ennnvvvy <deposoyivanrey_bsit@plmun.edu.ph>
2023-04-26 14:32:20 +0800

    changed app and _init_

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import render_template
-from hris import create_app
+from hris import app
 
 # checks if the app.py file has executed directly and not imported
 if __name__ == '__main__':
-
-    app = create_app()
-
     @app.errorhandler(404)
     def page_not_found(e):
         return render_template('404.html'), 404
--- hris/__init__.py
+++ hris/__init__.py
@@ -14,67 +14,62 @@ migrate = Migrate()
 DB_NAME = "hris"
 UPLOAD_FOLDER = 'static/upload'
 
+app = Flask(__name__)
 
-def create_app():
-   app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fa282f581ec1d84bc02c6f34d4da17a2'
+app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'images', 'uploads')
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 # 16MB in bytes
 
-   app.config['SECRET_KEY'] = 'fa282f581ec1d84bc02c6f34d4da17a2'
-   app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'images', 'uploads')
-   app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 # 16MB in bytes
+#for sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}.db"
 
-   #for sqlite
-   app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}.db"
+#for mysql
+#app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://root:@localhost:3306/{DB_NAME}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-   #for mysql
-   #app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://root:@localhost:3306/{DB_NAME}"
-   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db.init_app(app)
 
-   db.init_app(app)
+#import blueprints
+from .announcements.announcement import announcement_bp
+from .attendance.attendance import attendance_bp
+from .auth.auth import auth_bp
+from .departments.departments import departments_bp
+from .employees.employees import employees_bp
+from .home.home import home_bp
+from .payroll.payroll import payroll_bp
+from .payslips.payslips import payslips_bp
+from .profile.profile import profile_bp
+from .salaries.salaries import salaries_bp
+from .schedules.schedules import schedules_bp
 
-   #import blueprints
-   from .announcements.announcement import announcement_bp
-   from .attendance.attendance import attendance_bp
-   from .auth.auth import auth_bp
-   from .departments.departments import departments_bp
-   from .employees.employees import employees_bp
-   from .home.home import home_bp
-   from .payroll.payroll import payroll_bp
-   from .payslips.payslips import payslips_bp
-   from .profile.profile import profile_bp
-   from .salaries.salaries import salaries_bp
-   from .schedules.schedules import schedules_bp
-
-   #register Blueprints
-   app.register_blueprint(home_bp, url_prefix='/')
-   app.register_blueprint(auth_bp, url_prefix='/')
-   app.register_blueprint(departments_bp, url_prefix='/')
-   app.register_blueprint(employees_bp, url_prefix='/')
-   app.register_blueprint(announcement_bp, url_prefix='/')
-   app.register_blueprint(attendance_bp, url_prefix='/')
-   app.register_blueprint(salaries_bp, url_prefix='/')
-   app.register_blueprint(payroll_bp, url_prefix='/')
-   app.register_blueprint(payslips_bp, url_prefix='/')
-   app.register_blueprint(profile_bp, url_prefix='/')
-   app.register_blueprint(schedules_bp, url_prefix='/')
+#register Blueprints
+app.register_blueprint(home_bp, url_prefix='/')
+app.register_blueprint(auth_bp, url_prefix='/')
+app.register_blueprint(departments_bp, url_prefix='/')
+app.register_blueprint(employees_bp, url_prefix='/')
+app.register_blueprint(announcement_bp, url_prefix='/')
+app.register_blueprint(attendance_bp, url_prefix='/')
+app.register_blueprint(salaries_bp, url_prefix='/')
+app.register_blueprint(payroll_bp, url_prefix='/')
+app.register_blueprint(payslips_bp, url_prefix='/')
+app.register_blueprint(profile_bp, url_prefix='/')
+app.register_blueprint(schedules_bp, url_prefix='/')
 
 
-   # from .models import Users, EmployeeInfo, Attendance, Leave, EmploymentInfo, Positions, Departments
+# from .models import Users, EmployeeInfo, Attendance, Leave, EmploymentInfo, Positions, Departments
 
-   # for sqlite
-   if not path.exists('hris/instance' + DB_NAME):
-      with app.app_context():
-         db.create_all()
-   
-   # for mysql
-   # uncomment when creating new db.
-   # with app.app_context():
-   #    db.create_all()
- 
-   bcrypt.init_app(app)
-   migrate.init_app(app, db)
-   login_manager.login_view = 'auth_bp.login'
-   login_manager.login_message_category = 'info'
-   login_manager.init_app(app)
-   
-   return app
+# for sqlite
+if not path.exists('hris/instance' + DB_NAME):
+   with app.app_context():
+      db.create_all()
 
+# for mysql
+# uncomment when creating new db.
+# with app.app_context():
+#    db.create_all()
+
+bcrypt.init_app(app)
+migrate.init_app(app, db)
+login_manager.login_view = 'auth_bp.login'
+login_manager.login_message_category = 'info'
+login_manager.init_app(app)
