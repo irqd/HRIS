ac82ac3d241609955aba694d70481b53226c88cc
Ennnvvvy <deposoyivanrey_bsit@plmun.edu.ph>
2023-04-26 14:20:00 +0800

    edited python _init_

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import render_template
-
 from hris import create_app
-
-app = create_app()
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(413)
-def request_entity_too_large(error):
-    return render_template('413.html'), 413
-
 
 # checks if the app.py file has executed directly and not imported
 if __name__ == '__main__':
+
+    app = create_app()
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(413)
+    def request_entity_too_large(error):
+        return render_template('413.html'), 413
+    
     app.run(debug=True, threaded=True) 
